9ec1c6b35e955c35eb58f2cae5770fe8c62165aa
Z-eddy <576172400@qq.com>
2020-06-28 23:03:21 +0800

    Array sort排序 reverse反转

--- JSPractice/material/temp0.js
+++ JSPractice/material/temp0.js
@@ -1,6 +1,25 @@
-var arr=[];
-for(var i=0;i!=10;++i){
-    arr.push(i);//推入值
+var arr = [0, 1, 5, 10, 15];
+
+function compare(num0, num1) {
+    var delta = num0 - num1;
+    var ok = 0;
+    switch (delta) {
+        case delta < 0:
+            ok = -1;
+            break;
+        case delta > 0:
+            ok = 1;
+            break;
+        default:
+            break;
+    }
+    return ok;
 }
-arr.shift();//弹出第1个,先入先出
+
+function sub(num0,num1){
+    return num0-num1;
+}
+
+arr.sort(sub);
+//arr.reverse()
 alert(arr.join(" "));
