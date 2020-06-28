007624f61fbc12ca03504bf11c79a6f1037a76e5
Z-eddy <576172400@qq.com>
2020-06-28 21:55:39 +0800

    array自动增加中间未初始化的值为undefined

--- JSPractice/material/temp0.js
+++ JSPractice/material/temp0.js
@@ -1,6 +1,6 @@
-var arr=[1,2,3,4,5]
+var arr=[1,2]
 for (let index = 0; index < arr.length; ++index) {
     const element = arr[index];
 }
 arr[9]="test"
-alert(arr[8] + " " + arr[9])
+alert(arr[8])
