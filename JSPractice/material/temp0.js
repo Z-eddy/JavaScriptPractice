db5eb9254590ddc1b62680c806cd51283155d4e2
Z-eddy <576172400@qq.com>
2020-06-28 23:11:17 +0800

    Array slice(2,4) 返回subArray[2,4)的值

--- JSPractice/material/temp0.js
+++ JSPractice/material/temp0.js
@@ -1,4 +1,6 @@
 var arr = [0, 1, 5, 10, 15];
 
-var arr1=arr.concat(110,[111,112,113]);//创建副本
+var arr1=arr.slice(2,4);//返回[2,4)的索引
 alert(arr1.join(" "));
+var arr2=arr.slice(1);//返回1到end的索引
+alert(arr2.join(" "));
