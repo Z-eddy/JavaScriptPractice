cb7f4ccf9413005d8693493f7707035238a8ef84
Z-eddy <576172400@qq.com>
2020-06-28 23:08:33 +0800

    Arrar concat创建并合并数组

--- JSPractice/material/temp0.js
+++ JSPractice/material/temp0.js
@@ -1,25 +1,4 @@
 var arr = [0, 1, 5, 10, 15];
 
-function compare(num0, num1) {
-    var delta = num0 - num1;
-    var ok = 0;
-    switch (delta) {
-        case delta < 0:
-            ok = -1;
-            break;
-        case delta > 0:
-            ok = 1;
-            break;
-        default:
-            break;
-    }
-    return ok;
-}
-
-function sub(num0,num1){
-    return num0-num1;
-}
-
-arr.sort(sub);
-//arr.reverse()
-alert(arr.join(" "));
+var arr1=arr.concat(110,[111,112,113]);//创建副本
+alert(arr1.join(" "));
