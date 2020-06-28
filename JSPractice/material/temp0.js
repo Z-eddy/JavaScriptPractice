a9a02ad7866bdfdcd568c4ac541b39ef04ca2254
Z-eddy <576172400@qq.com>
2020-06-28 22:03:43 +0800

    array在最后附加元素

--- JSPractice/material/temp0.js
+++ JSPractice/material/temp0.js
@@ -1,6 +1,10 @@
-var arr=[1,2]
-for (let index = 0; index < arr.length; ++index) {
-    const element = arr[index];
+var arr=[];
+for (var i = 0; i != 10; ++i) {
+    arr[arr.length]=i;//每次在最后添加1
 }
-arr[9]="test"
-alert(arr[8])
+
+var s="";
+for (let i = 0; i !=arr.length; ++i) {
+    s += i !== arr.length - 1 ? i + " " : i;//最后一个不加空格
+}
+alert(s);
