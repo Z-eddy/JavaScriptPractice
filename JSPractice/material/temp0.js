69e65d03c6892cc7f9cf5912362c63aeb69add59
zr <576172400@qq.com>
2020-06-28 11:07:13 +0800

    标签指定循环,循环内部停止外部循环

--- JSPractice/material/temp0.js
+++ JSPractice/material/temp0.js
@@ -1,8 +1,13 @@
 function foo(){
-    var s="Hi";
-    if(s)
-        alert("string");
-    var n=12.5;
-    if(n)
-        alert("number")
+    var n = 0;
+    iCircle:
+    for (var i = 0; i < 10; ++i) {
+        for (var j = 0; j < 10; ++j) {
+            if(j==5&&i==5){
+                break iCircle;
+            }
+            ++n;
+        }
+    }
+    alert(n);
 }
