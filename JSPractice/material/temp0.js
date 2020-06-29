88550ffa882c3e1221d9ef189846e898bfb36d1f
zr <576172400@qq.com>
2020-06-29 10:28:48 +0800

    Array reduce reduceRight归约
    reduce正序,索引从0开始.
    reduceRight逆序

--- JSPractice/material/temp0.js
+++ JSPractice/material/temp0.js
@@ -1,23 +1,7 @@
-var arr = [0, 1, 5, 10, 15];
-//迭代方法,必须有3个参数:项,项索引,数组对象
-var everyResult = arr.every(function (item, index, array) {//every返回所有项为真
-    return item > 2;
-});//false
+var arr=[1,2,3,4,5];
+//reduceRight逆序,其他和reduce一样
+var v=arr.reduce(function(previousVal,currentVal,curIndex,array){
+    return previousVal+currentVal;//当前结果作为下一次函数的previousVal值
+});//第一次运行迭代,previousVal=1,currentVal=2
 
-var someResult = arr.some(function (item, index, array) {//some返回任意一项为真
-    return item > 2;
-});//true
-
-var filterResult = arr.filter(function (item, index, array) {//filter返回为真的数组项
-    return item > 2;
-});//5,10,15
-
-arr.forEach(function (item, index, array) {//forEach无返回值,对每一项做某个操作
-    array[index] = item + 1;
-});
-
-var mapResule = arr.map(function (item, index, array) {//map返回每一项运行结果的数组
-    return item + 2;
-});
-
-alert(mapResule);
+alert(v);
