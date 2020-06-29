b8b42a4810b3ef050017ca76ab06377c6118dcbc
zr <576172400@qq.com>
2020-06-29 17:43:01 +0800

    RegExp exec g全局多次匹配
    不加入g全局,则一直从头开始

--- JSPractice/material/temp0.js
+++ JSPractice/material/temp0.js
@@ -1,3 +1,14 @@
-var rgex=null;
-rgex = /\[bc\]at/g;//g表示全局,i不区分大小写,m多行匹配
-alert(rgex.source);//source保存规范的字面量所用的字符串
+var pattern=/.at/g;//不标注g全局,每次从0开始
+var text="cat,bat,sat";
+
+var matches = pattern.exec(text);
+alert(matches.index);//匹配到的索引         0
+alert(matches.input);//被测试的字符串       cat,bat,sat
+alert(matches[0]);//匹配到的值              cat
+alert(pattern.lastIndex);//下一次匹配位置   3
+
+var matches = pattern.exec(text);
+alert(matches.index);//匹配到的索引         4
+alert(matches.input);//被测试的字符串       cat,bat,sat
+alert(matches[0]);//匹配到的值              bat
+alert(pattern.lastIndex);//下一次匹配位置   7
