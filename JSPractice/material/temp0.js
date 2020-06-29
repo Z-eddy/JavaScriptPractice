8a1a3f90f22749a36403c334539b6090c6d26d81
zr <576172400@qq.com>
2020-06-29 10:18:05 +0800

    Array迭代方法every some filter forEach map

--- JSPractice/material/temp0.js
+++ JSPractice/material/temp0.js
@@ -1,11 +1,23 @@
 var arr = [0, 1, 5, 10, 15];
-var ind=arr.indexOf(5,3);//指定的起始索引超过了,找不到就返回-1
-alert(ind);//-1
+//迭代方法,必须有3个参数:项,项索引,数组对象
+var everyResult = arr.every(function (item, index, array) {//every返回所有项为真
+    return item > 2;
+});//false
 
-var obj={
-    name:"NameA"
-};
-var objArr = [{ name: "NameA" }];
-var objArr1=[obj];
-alert(objArr.indexOf(obj));//-1
-alert(objArr1.indexOf(obj));//0
+var someResult = arr.some(function (item, index, array) {//some返回任意一项为真
+    return item > 2;
+});//true
+
+var filterResult = arr.filter(function (item, index, array) {//filter返回为真的数组项
+    return item > 2;
+});//5,10,15
+
+arr.forEach(function (item, index, array) {//forEach无返回值,对每一项做某个操作
+    array[index] = item + 1;
+});
+
+var mapResule = arr.map(function (item, index, array) {//map返回每一项运行结果的数组
+    return item + 2;
+});
+
+alert(mapResule);
