784afff9c6cdd0b4e58b231d86e3eef342ccb329
zr <576172400@qq.com>
2020-06-29 18:01:04 +0800

    RegExp 有toString/toLocalString两个属性,返回字面值

--- JSPractice/material/temp0.js
+++ JSPractice/material/temp0.js
@@ -1,14 +1,5 @@
-var pattern=/.at/g;//不标注g全局,每次从0开始
+var pattern0=/.at/g;//不标注g全局,每次从0开始
+var pattern1=new RegExp(".at","g");
 var text="cat,bat,sat";
 
-var matches = pattern.exec(text);
-alert(matches.index);//匹配到的索引         0
-alert(matches.input);//被测试的字符串       cat,bat,sat
-alert(matches[0]);//匹配到的值              cat
-alert(pattern.lastIndex);//下一次匹配位置   3
-
-var matches = pattern.exec(text);
-alert(matches.index);//匹配到的索引         4
-alert(matches.input);//被测试的字符串       cat,bat,sat
-alert(matches[0]);//匹配到的值              bat
-alert(pattern.lastIndex);//下一次匹配位置   7
+alert(pattern1.toString());
